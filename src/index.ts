2576c9d022886184a8adeb2c44b85cdf989e52b7
Julien Barbay <julien.barbay@linecorp.com>
2019-12-14 11:21:56 +0900

    edgesOf(node) : find edges by node (with optional direction)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+export const FROM = 'from'
+export const TO = 'to'
+
 export type Node = string
 
 export type Edge = {
-  from: Node,
-  to: Node,
+  [FROM]: Node,
+  [TO]: Node,
   cost: number,
 }
 
@@ -13,6 +16,8 @@ export type Graph = {
   edges: Edge[]
 }
 
+export type Direction = (typeof FROM | typeof TO)
+
 
 
 let graph: Graph
@@ -21,4 +26,16 @@ let graph: Graph
 
 export const use = (context: Graph): void => {
   graph = context
+}
+
+
+
+export const edgesOf = (node: Node, direction?: Direction): Edge[] => {
+  if (!graph.nodes.includes(node))
+    throw new Error(`No Such Node - (${node})`)
+
+  if (!direction)
+    return graph.edges.filter(edge => edge[FROM] === node || edge[TO] === node)
+
+  return graph.edges.filter(edge => edge[direction] === node)
 }
--- tests/utilities.ts
+++ tests/utilities.ts
@@ -1,4 +1,5 @@
 import { load } from '~/mocks/load'
+import { use, edgesOf } from '@/index'
 
 export default () => describe('utilities', () => {
   test('load data', async () => {
@@ -6,5 +7,20 @@ export default () => describe('utilities', () => {
 
     expect(graph.nodes.length).not.toBe(0)
     expect(graph.edges.length).not.toBe(0)
+
+    use(graph)
+  })
+
+  test('find edges of a starting point (valid)', () => {
+    expect(edgesOf('A', 'from')).toEqual([
+      expect.objectContaining({ from: 'A', to: 'B' }),
+      expect.objectContaining({ from: 'A', to: 'C' }),
+      expect.objectContaining({ from: 'A', to: 'D' }),
+    ])
+  })
+
+  test('find edges of a starting point (invalid)', () => {
+    expect(() => edgesOf('Z'))
+      .toThrowError(/No Such Node/)
   })
 })
